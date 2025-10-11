daa7955c647efc97d3c27d64d0015302666e3b33
Maisara <maisara.sherif.ms@gmail.com>
2025-10-11 16:44:52 +0400

    update

--- order_service/repository/interface.go
+++ order_service/repository/interface.go
@@ -5,17 +5,18 @@ import (
 )
 
 type OrderRepository interface {
+	// Order operations
 	Create(order *data.Order) error
-
 	GetByID(id string) (*data.Order, error)
-
 	GetAll() ([]*data.Order, error)
-
 	GetByCustomerID(customerID string) ([]*data.Order, error)
-
 	UpdateStatus(id, status string) error
-
 	UpdatePaymentID(orderID, paymentID string) error
+
+	// Product operations
+	GetProduct(id string) (*data.Product, error)
+	GetAllProducts() ([]*data.Product, error)
+	GetAvailableProducts() ([]*data.Product, error)
 
 	Close() error
 }
